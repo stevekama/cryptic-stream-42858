b5fbdfd094c557dce7ad5f00eec683a8026455d1
stevekama <stevekamahertz@gmail.com>
2019-09-10 12:06:03 +0300

    paypal error check one

--- api/paypal_api/checkout.php
+++ api/paypal_api/checkout.php
@@ -5,31 +5,29 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-use \PayPal\Api\Payer;
-use \PayPal\Api\Item;
-use \PayPal\Api\ItemList;
-use \PayPal\Api\Details;
-use \PayPal\Api\Amount;
-use \PayPal\Api\Transaction;
-use \PayPal\Api\RedirectUrls;
-use \PayPal\Api\Payment;
+// use \PayPal\Api\Payer;
+// use \PayPal\Api\Item;
+// use \PayPal\Api\ItemList;
+// use \PayPal\Api\Details;
+// use \PayPal\Api\Amount;
+// use \PayPal\Api\Transaction;
+// use \PayPal\Api\RedirectUrls;
+// use \PayPal\Api\Payment;
 
-include_once '../../models/initialization.php';
+// include_once '../../models/initialization.php';
 
-// authenticate user 
-// get key and secret 
-$app = new Apps();
+// // authenticate user 
+// // get key and secret 
+// $app = new Apps();
 
-// get app details 
-$current_app = $app->find_by_token($_POST['token']);
+// // get app details 
+// $current_app = $app->find_by_token($_POST['token']);
 
-// Paypal Auth 
-$paypal_auth = new PayPalAuth($current_app['key'], $current_app['secret']);
+// // Paypal Auth 
+// $paypal_auth = new PayPalAuth($current_app['key'], $current_app['secret']);
 
-// get paypal details 
-$paypal = $paypal_auth->auth();
-
-echo $paypal;
+// // get paypal details 
+// $paypal = $paypal_auth->auth();
 // // process payments
 // // get post data
 // $product = $_POST['product'];
