c883b2acf7e5950403c20a0bb449295a222cbf56
stevekama <stevekamahertz@gmail.com>
2019-09-11 16:32:01 +0300

    save array

--- api/paypal_api/pay.php
+++ api/paypal_api/pay.php
@@ -56,25 +56,13 @@ try{
     die();
 }
 
-//save to db 
- // initialize payments 
- $db_payment = Payment::get($paymentId, $paypal);
-    
- //initialize paypal transation class
- $paypal_transactions = new PayPalTransactions();
- $paypal_transactions->app_token = $current_app['app_token'];
- $paypal_transactions->transaction_id = $db_payment->getId();
- $paypal_transactions->payment_amount = $db_payment->transactions[0]->amount->total;
- $paypal_transactions->payment_status = $db_payment->getState();
- $paypal_transactions->invoice_id = $db_payment->transactions[0]->invoice_number;
- $paypal_transactions->transaction_date = date('YmdHis');
-
- //save data in db 
- if($paypal_transactions->create()){
-     // redirect to the users response url
-     redirect_to($current_app['response_url']);
- }else{
-     $data['message'] = 'Error in storing data';
-     echo json_encode($data);
-     die();
- }
+//save to db    
+//initialize paypal transation class
+$record_data = array();
+$record_data['app_token'] = $current_app['app_token'];
+$record_data['transaction_id'] = $payment->getId();
+$record_data['payment_amount'] = $payment->transactions[0]->amount->total;
+$record_data['payment_status'] = $payment->getState();
+$record_data['invoice_id'] = $payment->transactions[0]->invoice_number;
+$record_data['transaction_date'] = date('YmdHis');
+echo json_encode($record_data);
