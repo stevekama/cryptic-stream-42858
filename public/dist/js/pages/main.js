3c624957cc453aa46add8072e196cf91f6d110c2
stevekama <stevekamahertz@gmail.com>
2019-09-12 12:50:57 +0300

    read apps

--- public/dist/js/pages/main.js
+++ public/dist/js/pages/main.js
@@ -14,8 +14,24 @@ $(document).ready(function(){
                         }
                     }
                 }
-                console.log(app_data);
-                console.table(data);
+                var create_div = '';
+                for (var i = 0; i < data.length; i++) {
+                    create_div += '<div class="col-lg-3 col-xs-6">';
+                    create_div += '<div class="small-box bg-green">';
+                    create_div += '<div class="inner">';
+                    create_div += '<h3>&nbsp;</h3>';
+                    create_div += '<p>Transactions</p>';
+                    create_div += '</div>';
+                    create_div += '<div class="icon">';
+                    create_div += '<i class="ion ion-bag"></i>';
+                    create_div += '</div>';
+                    create_div += '<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>';
+                    create_div += '</div>';
+                    create_div += '</div>';
+                }
+
+                $('#apps_data').append(create_div);
+        
             }
         });
     }
