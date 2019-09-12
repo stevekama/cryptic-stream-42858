e70eecd454935690ad53305a100b3df54c0437b4
stevekama <stevekamahertz@gmail.com>
2019-09-12 13:06:14 +0300

    rea apps

--- public/dist/js/pages/main.js
+++ public/dist/js/pages/main.js
@@ -6,35 +6,9 @@ $(document).ready(function(){
             type:'POST',
             dataType:'json',
             success:function(data){
-                var app_data = [];
-                for (var i = 0; i < data.length; i++) {
-                    for (var key in data[i]) {
-                        if (app_data.indexOf(key) === -1) {
-                            app_data.push(key);
-                        }
-                    }
-                }
-                var create_div = '';
-                for (var i = 0; i < data.length; i++) {
-                    create_div += '<div class="col-lg-3 col-xs-6">';
-                    create_div += '<div class="small-box bg-green">';
-                    create_div += '<div class="inner">';
-                    create_div += '<h3>&nbsp;</h3>';
-                    for (var j = 0; j < app_data.length; j++) {
-                        console.log(data[i][col[j]]); 
-                    }
-                    create_div += '<p>'+data.app_name+'</p>';
-                    create_div += '</div>';
-                    create_div += '<div class="icon">';
-                    create_div += '<i class="ion ion-bag"></i>';
-                    create_div += '</div>';
-                    create_div += '<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>';
-                    create_div += '</div>';
-                    create_div += '</div>';
-                }
-
-                $('#apps_data').append(create_div);
-        
+                $.each(data, function( key, value ) {
+                    alert( key + ": " + value );
+                });
             }
         });
     }
