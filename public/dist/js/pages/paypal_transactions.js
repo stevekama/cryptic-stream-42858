f39e713f4c2fabea5f3dc2e8372c10d6bac7e710
stevekama <stevekamahertz@gmail.com>
2019-09-13 15:19:59 +0300

    remove datatables

--- public/dist/js/pages/paypal_transactions.js
+++ public/dist/js/pages/paypal_transactions.js
@@ -20,12 +20,5 @@ $(document).ready(function(){
         });
     }
     fetch_transactions();
-    $('#payPalTrns').DataTable({
-        'paging'      : true,
-        'lengthChange': false,
-        'searching'   : false,
-        'ordering'    : true,
-        'info'        : true,
-        'autoWidth'   : false
-    })
+    
 });
