787bf848f9f0b9e0d6b54911ceff0b901d5153e9
stevekama <stevekamahertz@gmail.com>
2019-09-13 11:03:06 +0300

    transactions error

--- api/paypal_api/fetch.php
+++ api/paypal_api/fetch.php
@@ -20,7 +20,7 @@ $trns->user_id = $session->user_id;
 
 // filter values 
 $count_trns = $trns->find_by_user_id();
-$totalFilter = $count_trns->rowCount();
+//$totalFilter = $count_trns->rowCount();
 
 // Search 
 // $sql = "SELECT * FROM paypal_transactions INNER JOIN apps ON paypal_transactions.app_token = apps.app_token WHERE paypal_transactions.user_id = '{$user_id}' ";
