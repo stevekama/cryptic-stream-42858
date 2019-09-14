8bce875d04e2e4d031cc0ef867788f1e77a52043
stevekama <stevekamahertz@gmail.com>
2019-09-14 12:52:21 +0300

    access token

---
+++ api/m_api/access_token.php
@@ -0,0 +1,23 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+$consumerKey = $_POST['key']; //Fill with your app Consumer Key
+$consumerSecret = $_POST['secret']; // Fill with your app Secret
+$headers = ['Content-Type:application/json; charset=utf8'];
+$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+$access_token = $result->access_token;
+echo $access_token;
+
+curl_close($curl);
