7ab872f754f30de87ffadc99f255c77812339d5e
stevekama <stevekamahertz@gmail.com>
2019-09-15 09:52:28 +0300

    access token using auth

--- api/m_api/access_token.php
+++ api/m_api/access_token.php
@@ -7,17 +7,9 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 
 $consumerKey = $_POST['key']; //Fill with your app Consumer Key
 $consumerSecret = $_POST['secret']; // Fill with your app Secret
-$headers = ['Content-Type:application/json; charset=utf8'];
-$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_HEADER, FALSE);
-curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
-$result = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-$result = json_decode($result);
-$access_token = $result->access_token;
-echo $access_token;
 
-curl_close($curl);
+$app = new Auth($consumerKey, $consumerSecret);
+
+$access_token = $app->Access_Token();
+
+echo $access_token;
