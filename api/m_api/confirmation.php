1c29bdde67ec9edea341b7bff66738e2f023661f
stevekama <stevekamahertz@gmail.com>
2019-09-15 11:50:58 +0300

    confirmation url

---
+++ api/m_api/confirmation.php
@@ -0,0 +1,36 @@
+<?php 
+header("Content-Type: application/json");
+
+include_once '../../models/initialization.php';
+
+$response = '{
+    "ResultCode": 0, 
+    "ResultDesc": "Confirmation Received Successfully"
+}';
+
+// DATA
+$mpesaResponse = file_get_contents('php://input');
+$jsonMpesaResponse = json_decode($mpesaResponse, true);
+
+// Save Data In DB
+// 1.initialize mpesa class
+$trns = new MPESATransactions();
+$trns->transaction_type   = '';
+$trns->transaction_id     = '';
+$trns->transaction_time   = '';
+$trns->transaction_amount = '';
+$trns->business_shortcode = '';
+$trns->bill_refnumber     = '';
+$trns->invoice_number     = '';
+$trns->original_balance   = '';
+$trns->third_party_transaction_id = '';
+$trns->msisdn             = '';
+$trns->first_name         = '';
+$trns->middle_name        = '';
+$trns->last_name          = '';
+
+if($trns->create()){
+    echo json_encode(array('message'=>'success'));
+}
+
+echo $response;
