60a19d38fe7f384773289edccebb7d1bee178da3
stevekama <stevekamahertz@gmail.com>
2019-09-16 06:37:54 +0300

    validation url

---
+++ api/m_api/validation.php
@@ -0,0 +1,11 @@
+<?php 
+header("Content-Type: application/json");
+
+include_once '../../models/initialization.php';
+
+$response = '{
+    "ResultCode": 0, 
+    "ResultDesc": "Confirmation Received Successfully"
+}';
+
+echo $response;
