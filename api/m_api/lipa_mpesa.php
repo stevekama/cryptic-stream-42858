734245775ec9f34f19f498b29d51ab8f724e3a46
stevekama <stevekamahertz@gmail.com>
2019-09-16 15:51:27 +0300

    lipa mpesa

--- api/m_api/lipa_mpesa.php
+++ api/m_api/lipa_mpesa.php
@@ -16,17 +16,19 @@ $app = new Auth($consumerKey, $consumerSecret);
 
 // 2. declare lipa na mpesa variables
 $access_token      = $app->Access_Token();
-// bring in php date class function 
-$d = new DateTime();
+// // bring in php date class function 
+// $d = new DateTime();
 
-$BusinessShortCode = "174379";
-$Timestamp         = $d->format('Ymdhis');
-$Passkey           = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
-$Password          = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
-$Amount            = "1";
-$PartyA            = "254715356718";
-$PartyB            = $BusinessShortCode;
-$PhoneNumber       = "254715356718";
-$CallBackURL       = base_url()."api/m_api/callback_url.php";
-$AccountReference  = "CART098";
-$TransactionDesc   = "Buying a shoe";
+// $BusinessShortCode = "174379";
+// $Timestamp         = $d->format('Ymdhis');
+// $Passkey           = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+// $Password          = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+// $Amount            = "1";
+// $PartyA            = "254715356718";
+// $PartyB            = $BusinessShortCode;
+// $PhoneNumber       = "254715356718";
+// $CallBackURL       = base_url()."api/m_api/callback_url.php";
+// $AccountReference  = "CART098";
+// $TransactionDesc   = "Buying a shoe";
+
+echo $access_token;
