6d64e3a8fb20914c43accaa7f7e34adb4025191d
stevekama <stevekamahertz@gmail.com>
2019-09-16 16:12:08 +0300

    lipa na mpesa

--- api/m_api/lipa_mpesa.php
+++ api/m_api/lipa_mpesa.php
@@ -35,4 +35,34 @@ $CallBackURL       = base_url()."api/m_api/callback_url.php";
 $AccountReference  = "CART098";
 $TransactionDesc   = "Buying a shoe";
 
-echo $access_token;
+// 3. run lipa na mpesa code 
+$url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token)); //setting custom header
+
+$curl_post_data = array(
+  //Fill in the request parameters with valid values
+  'BusinessShortCode' => $BusinessShortCode,
+  'Password' => $Password,
+  'Timestamp' => $Timestamp,
+  'TransactionType' => 'CustomerPayBillOnline',
+  'Amount"' => $Amount,
+  'PartyA' => $PartyA,
+  'PartyB' => $PartyB,
+  'PhoneNumber' => $PhoneNumber,
+  'CallBackURL' => $CallBackURL,
+  'AccountReference' => $AccountReference,
+  'TransactionDesc' => $TransactionDesc
+);
+
+$data_string = json_encode($curl_post_data);
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+$curl_response = curl_exec($curl);
+// print_r($curl_response);
+echo $curl_response;
