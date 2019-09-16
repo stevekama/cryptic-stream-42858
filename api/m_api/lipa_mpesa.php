1d05626348d85f22bcf63a5683bef4b7450ca6f2
stevekama <stevekamahertz@gmail.com>
2019-09-16 16:02:48 +0300

    lipa na mpesa

--- api/m_api/lipa_mpesa.php
+++ api/m_api/lipa_mpesa.php
@@ -5,3 +5,19 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../models/initialization.php';
+
+// 1. obtain access token 
+
+$consumerKey = $_POST['key']; //Fill with your app Consumer Key
+
+$consumerSecret = $_POST['secret']; // Fill with your app Secret
+
+// initialize mpesa auth class
+$app = new Auth($consumerKey, $consumerSecret);
+
+// 2. declare lipa na mpesa variables
+$access_token      = $app->Access_Token();
+
+echo $access_token;
