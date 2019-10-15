ac8b6bb46fda848404ccfd05c8862a64d872c76e
matibejeremy <matibejeremy@gmail.com>
2019-10-15 05:28:33 +0300

    DB Configuration

--- config/database.php
+++ config/database.php
@@ -1,16 +1,16 @@
 <?php 
 class Database{
-     private $host = 'us-cdbr-iron-east-02.cleardb.net';
-     private $username = 'b23c5653b9b54b';
-     private $password = 'e4b03cf3';
-     private $dbname = 'heroku_bf3db7c0a0aaab6';
+     private $host = '34.215.237.227';
+     private $username = 'api';
+     private $password = '12345';
+     private $dbname = 'cubes';
      private $conn;
 
      public function connect()
      {
           $this->conn = null;
           try{
-               $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
+               $this->conn = new PDO('pgsql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
           }catch(PDOException $e){
