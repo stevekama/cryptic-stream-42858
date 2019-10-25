841869170fbc527bf1a0d652dc4920c3206673ac
stevekama <stevekamahertz@gmail.com>
2019-10-25 10:48:55 +0100

    fetch customer gender

---
+++ api/customer_gender/fetch.php
@@ -0,0 +1,44 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../models/initialization.php';
+
+$gender = new Customer_Gender();
+
+if($_POST['action'] == 'FETCH_ALL'){
+
+    $customer_genders = $gender->fetch_all();
+
+    $num_cust_genders = $customer_genders->rowCount();
+
+    $data = array();
+
+    if($num_cust_genders > 0){
+
+        while($customer_gender = $customer_genders->fetch(PDO::FETCH_ASSOC)){
+
+            extract($customer_gender);
+
+            $customer_gender_item = array(
+                'id'                        => $id,
+                'gender'          => $gender,
+                'created_date'    => $created_date,
+                'created_user_id' => $created_user_id,
+                'edited_date'     => $edited_date,
+                'edited_user_id'  => $edited_user_id,
+            );
+    
+            // push to array 
+            array_push($data, $customer_doc_item);
+        }
+
+    }else{
+        $data['message'] = 'empty';
+    }
+    
+    echo json_encode($data);
+}
