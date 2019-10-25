0aec32afa0db707b6c72c09391e7c23ef8f38469
stevekama <stevekamahertz@gmail.com>
2019-10-25 09:23:00 +0100

    fetch customer type data

--- api/customer_type/fetch.php
+++ api/customer_type/fetch.php
@@ -7,4 +7,37 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 
 include_once '../../models/initialization.php';
 
-$customer_type = new Customer_Type();
+$types = new Customer_Type();
+
+if($_POST['action'] == 'FETCH_ALL'){
+
+    $customer_types = $types->fetch_all();
+
+    $num_cust_types = $customer_types->rowCount();
+
+    $data = array();
+
+    if($num_cust_types > 0){
+
+        while($customer_type = $customer_types->fetch(PDO::FETCH_ASSOC)){
+
+            extract($customer_type);
+
+            $customer_type_item = array(
+                'cust_type'             => $cust_type,
+                'created_date'          => $created_date,
+                'created_user_id'       => $created_user_id,
+                'edited_date'           => $edited_date,
+                'edited_user_id'        => $edited_user_id,
+            );
+    
+            // push to array 
+            array_push($data, $customer_type_item);
+        }
+
+    }else{
+        $data['message'] = 'empty';
+    }
+    
+    echo json_encode($data);
+}
