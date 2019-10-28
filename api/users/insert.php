bfb536d044068e56e1b15dc7f98ae6af0a8547bf
stevekama <stevekamahertz@gmail.com>
2019-10-28 10:43:26 +0100

    find customer by customer id

--- api/users/insert.php
+++ api/users/insert.php
@@ -8,33 +8,34 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 include_once '../../models/initialization.php';
 
 $user = new Users();
+$cust = new Customers();
 
 if($_POST['password'] === $_POST['confirm']){
     
     // find customer by customer is 
-    $cust = new Customers();
     $customer = $cust->find_by_id($_POST['customer_id']);
-    $user->fullnames   = $customer->first_name.' '.$customer->other_names;
-    $user->phone       = $customer->phone_number;
-    $user->email       = $customer->email_address;
-    $user->username    = $_POST['username'];
-    $user->password    = $_POST['password'];
-    $user->customer_id = $customer->id;
+    echo json_encode($customer);
+    // $user->fullnames   = $customer->first_name.' '.$customer->other_names;
+    // $user->phone       = $customer->phone_number;
+    // $user->email       = $customer->email_address;
+    // $user->username    = $_POST['username'];
+    // $user->password    = $_POST['password'];
+    // $user->customer_id = $customer->id;
 
-    //find user by email 
-    $usermail = $user->find_user_by_email($user->email);
+    // //find user by email 
+    // $usermail = $user->find_user_by_email($user->email);
 
-    if($usermail){
-        echo json_encode(array('message'=>'emailError'));
-        die();
-    }
+    // if($usermail){
+    //     echo json_encode(array('message'=>'emailError'));
+    //     die();
+    // }
 
-    ///create user 
-    if($user->create()){
-        echo json_encode(array('message'=>'success'));
-    }else{
-        echo json_encode(array('message'=>'failed'));
-    }
+    // ///create user 
+    // if($user->create()){
+    //     echo json_encode(array('message'=>'success'));
+    // }else{
+    //     echo json_encode(array('message'=>'failed'));
+    // }
 }else{
     echo json_encode(array('message'=>'errorPass'));
 }
