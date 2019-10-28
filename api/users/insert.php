508f8a34a55734d5afbc1da87ac473a9dfd97943
stevekama <stevekamahertz@gmail.com>
2019-10-28 10:46:44 +0100

    populate cusomer into users table

--- api/users/insert.php
+++ api/users/insert.php
@@ -14,28 +14,28 @@ if($_POST['password'] === $_POST['confirm']){
     
     // find customer by customer is 
     $customer = $cust->find_by_id($_POST['customer_id']);
-    echo json_encode($customer);
-    // $user->fullnames   = $customer->first_name.' '.$customer->other_names;
-    // $user->phone       = $customer->phone_number;
-    // $user->email       = $customer->email_address;
-    // $user->username    = $_POST['username'];
-    // $user->password    = $_POST['password'];
-    // $user->customer_id = $customer->id;
+    //echo json_encode($customer);
+    $user->fullnames   = $customer['first_name'].' '.$customer['other_names'];
+    $user->phone       = $customer['phone_number'];
+    $user->email       = $customer['email_address'];
+    $user->username    = $_POST['username'];
+    $user->password    = $_POST['password'];
+    $user->customer_id = $customer->id;
 
-    // //find user by email 
-    // $usermail = $user->find_user_by_email($user->email);
+    //find user by email 
+    $usermail = $user->find_user_by_email($user->email);
 
-    // if($usermail){
-    //     echo json_encode(array('message'=>'emailError'));
-    //     die();
-    // }
+    if($usermail){
+        echo json_encode(array('message'=>'emailError'));
+        die();
+    }
 
-    // ///create user 
-    // if($user->create()){
-    //     echo json_encode(array('message'=>'success'));
-    // }else{
-    //     echo json_encode(array('message'=>'failed'));
-    // }
+    ///create user 
+    if($user->create()){
+        echo json_encode(array('message'=>'success'));
+    }else{
+        echo json_encode(array('message'=>'failed'));
+    }
 }else{
     echo json_encode(array('message'=>'errorPass'));
 }
