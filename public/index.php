62e300b70139a3248b6424fdbf725f8a33944643
stevekama <stevekamahertz@gmail.com>
2019-10-30 13:00:59 +0300

    One side of the dashbord

--- public/index.php
+++ public/index.php
@@ -7,13 +7,51 @@ require_once('layouts/systems/header.php'); ?>
     <small>Version 2.0</small>
   </h1>
   <ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo base_url(); ?>/public/index.php"><i class="fa fa-dashboard"></i> Home</a></li>
     <li class="active">Dashboard</li>
   </ol>
 </section>
 
 <!-- Main content -->
 <section class="content">
+  <div class="row">
+    <div class="col-lg-7">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="glyphicon glyphicon-euro"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Total Transactions</span>
+              <span class="info-box-number"><small>$</small>900</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="glyphicon glyphicon-file"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Wallet</span>
+              <span class="info-box-number"><small>$</small>2000</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+      </div>
+    </div>
+
+    <div class="col-lg-5">
+      
+    </div>
+  </div>
 
 </section>
 <!-- /.content -->
