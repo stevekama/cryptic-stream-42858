92d89d63ae18ba60d246e4ffb4ddb19ac007d5c0
stevekama <stevekamahertz@gmail.com>
2019-10-30 14:19:50 +0300

    added new column lenght

--- public/index.php
+++ public/index.php
@@ -104,7 +104,7 @@ require_once('layouts/systems/header.php'); ?>
         <!-- right col (We are only adding the ID to make the widgets sortable)-->
         <section class="col-lg-5 connectedSortable">
           <div class="row">
-            <div class="col-lg-5 col-xs-12">
+            <div class="col-lg-6 col-xs-12">
               <!-- small box -->
               <div class="small-box bg-aqua">
                 <div class="inner">
@@ -112,14 +112,14 @@ require_once('layouts/systems/header.php'); ?>
                   <p>Transactions</p>
                 </div>
                 <div class="icon">
-                  <i class="glyphicon glyphicon-euro"></i>
+                  <i class="ion ion-stats-bars"></i>
                 </div>
                 <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
 
-            <div class="col-lg-5 col-xs-6">
+            <div class="col-lg-6 col-xs-12">
               <!-- small box -->
               <div class="small-box bg-green">
                 <div class="inner">
@@ -128,7 +128,7 @@ require_once('layouts/systems/header.php'); ?>
                   <p>Wallet</p>
                 </div>
                 <div class="icon">
-                  <i class="glyphicon glyphicon-hdd"></i>
+                  <i class="ion ion-pie-graph"></i>
                 </div>
                 <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
               </div>
