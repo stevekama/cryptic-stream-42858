a677733fb367574a07cc8ceadce94cc4859e55d3
stevekama <stevekamahertz@gmail.com>
2019-10-30 14:51:18 +0300

    remove collapsibility

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ require_once('layouts/systems/header.php'); ?>
       <!-- Main row -->
       <div class="row">
         <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
+        <section class="col-lg-7">
           <!--Transactions Graph-->
           <div class="col-md-12">
             <div class="box">
@@ -46,7 +46,7 @@ require_once('layouts/systems/header.php'); ?>
         </section>
         <!-- /.Left col -->
         <!-- right col (We are only adding the ID to make the widgets sortable)-->
-        <section class="col-lg-5 connectedSortable">
+        <section class="col-lg-5">
           <div class="row">
             <div class="col-lg-12 col-xs-12">
               <!-- small box -->
