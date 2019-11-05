a2ddc5a582746d5f10ce529499d14188fa906969
stevekama <stevekamahertz@gmail.com>
2019-11-05 11:47:48 +0300

    user frontend profile

--- public/layouts/systems/header.php
+++ public/layouts/systems/header.php
@@ -98,7 +98,7 @@
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                        <a href="<?php echo base_url(); ?>public/users/index.php" class="btn btn-default btn-flat">Profile</a>
                                     </div>
                                     <div class="pull-right">
                                         <a href="#" class="btn btn-default btn-flat logout">Sign out</a>
