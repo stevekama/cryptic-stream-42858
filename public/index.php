9bc827e83e60ea6808ff9a359b102759b70db79d
stevekama <stevekamahertz@gmail.com>
2019-11-11 17:17:27 +0300

    dashboard sidebar

--- public/index.php
+++ public/index.php
@@ -56,10 +56,6 @@ require_once('layouts/systems/header.php'); ?>
                 </div>
                 <!-- /.col -->
                 <div class="col-md-4">
-                  <p class="text-center">
-                    <strong>Goal Completion</strong>
-                  </p>
-                  <div class="col-lg-3 col-xs-6">
                     <!-- small box -->
                     <div class="small-box bg-aqua">
                       <div class="inner">
@@ -72,8 +68,6 @@ require_once('layouts/systems/header.php'); ?>
                       </div>
                       <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
-                  </div>
-                  <!-- ./col -->
                   
 
                   <div class="progress-group">
