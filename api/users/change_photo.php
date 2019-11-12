94c08f8f72901c15718832a3fdc5c1c03df4b946
matibejeremy <matibejeremy@gmail.com>
2019-11-12 16:56:14 +0300

    user photo class

---
+++ api/users/change_photo.php
@@ -0,0 +1,20 @@
+<?php 
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../models/initialization.php';
+
+$user = new Users();
+
+// find user by id 
+$current_user = $user->find_user_by_id($_POST['user_id']);
+
+if(!$current_user){
+    echo json_encode(array('message'=>'errorUser'));
+    die();
+}
+
+$user->id          = $current_user['id'];
--- models/users.php
+++ models/users.php
@@ -143,7 +143,7 @@ class Users {
             // return $user;
             // change and update password
             $query = "UPDATE ".$this->table_name." SET password = :password WHERE id = :id";
-            //propare statement 
+            //prepare statement 
             $stmt = $this->conn->prepare($query);
 
             // hash password
@@ -161,4 +161,107 @@ class Users {
         }
         
     }
+
+    /// update user  profile 
+    private $temp_path;
+    protected $upload_dir = "img";
+
+    // store errors
+    public $errors = array();
+
+    //upload errors
+    protected $upload_errors = array(
+        //http://www.php.net/manual/en/features.file-upload.errors.php
+        UPLOAD_ERR_OK => "No errors.",
+        UPLOAD_ERR_INI_SIZE => "Large than upload_max_filesize",
+        UPLOAD_ERR_FORM_SIZE => "Large than form max_size",
+        UPLOAD_ERR_PARTIAL => "Partial upload",
+        UPLOAD_ERR_NO_FILE => "No file",
+        UPLOAD_ERR_NO_TMP_DIR => "No temporary directory",
+        UPLOAD_ERR_CANT_WRITE => "Cant write to disk",
+        UPLOAD_ERR_EXTENSION => "File upload stopped by extension. "
+    );
+
+    //attach file removing all errors
+    public function attach_file($file){
+        /*
+         * Perform error checking on the form parameters
+         * set object attributes to form parameters
+         * dont worry about saving anything to the database yet.
+         */
+        //perform error checking on the form parameters
+        if(!$file || empty($file) || !is_array($file)){
+            //error: nothing uploaded or wrong argument usage
+            $this->errors[] = "No file was uploaded. ";
+            return false;
+
+        }elseif($file['error'] != 0){
+            //error: report what PHP says went wrong
+            $this->errors[] = $this->upload_errors[$file['error']];
+            return false;
+        }else {
+            //Set object attributes to the form parameters
+            $this->temp_path = $file['tmp_name'];
+            $this->profile = basename(time().$file['name']);
+            //Dont worry about the databaseyet
+            return true;
+        }
+
+    }
+
+    public function save_photo(){
+        if(isset($this->id)){
+            /*
+            * Make sure there are no errors
+             * Attempt to move the file
+             * Save corresponding entry to the database
+             */
+            //1. make sure there are no errors
+
+            if(!empty($this->errors)){ return false; }
+
+            //2. cant sae without filename and tempt location
+            if(empty($this->profile) || empty($this->temp_path)){
+                $this->errors[] = "The file location was not available. ";
+                return false;
+            }
+
+            //3. Determine the target_path
+            $target_path = SITE_ROOT.DS.'public'.DS.'dist'.DS.$this->upload_dir.DS.$this->profile;
+            //return $target_path;
+
+            //4. make sure the file doesn't exist
+            if(file_exists($target_path)){
+                return unlink($target_path) ? true : false;
+            }
+
+            //5. Attempt to move the file
+            if(move_uploaded_file($this->temp_path, $target_path)){
+                // Success
+                $query = "UPDATE ".$this->table_name." SET profile = :profile WHERE id = :id";
+                //propare statement 
+                $stmt = $this->conn->prepare($query);
+
+                // bind profile pic 
+                $stmt->bindParam(':profile', $this->profile);
+
+                // Execute Query 
+                if($stmt->execute()){
+                    return true;
+                } 
+            }else{
+                /*
+                 * File was not moved
+                 */
+                $this->errors[] = "The file upload failed, possibly due to incorrect permissions on the uploaded folder.";
+                return false;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+
+
 }
