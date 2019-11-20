926efbb5ab65dfd94f2ac09b18f9db42ecf39e52
stevekama <stevekamahertz@gmail.com>
2019-11-20 14:58:42 +0300

    fetch customer wallet

--- api/customer_wallet/fetch.php
+++ api/customer_wallet/fetch.php
@@ -5,4 +5,16 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../models/initialization.php';
-?>
+
+$wallet = new Customer_Wallet();
+
+if($_POST['action'] == 'FETCH_FOR_CUSTOMER'){
+
+    $customer_wallet = $wallet->fetch_wallet_for_customer($_POST['customer_id']);
+
+    if(!$customer_wallet){
+        echo json_encode(array('message'=>'noWalletFound'));
+        die();
+    }
+    echo json_encode($customer_wallet);
+}
