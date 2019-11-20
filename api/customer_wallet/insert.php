64a306b1891fa539f18864f1e7dbc7c045a7bae5
stevekama <stevekamahertz@gmail.com>
2019-11-20 10:45:09 +0300

    load customer id

--- api/customer_wallet/insert.php
+++ api/customer_wallet/insert.php
@@ -5,14 +5,22 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../models/initialization.php';
-
+// initialize customer wallet.
 $wallet = new Customer_Wallet();
-
 $data = array();
-
 $wallet->user_id = $_POST['user_id'];
+// initialize customer
+$customer = new Customers();
+// customer id
 $wallet->customer_id = $_POST['customer_id'];
-$wallet->phone_number = $_POST['phone_number'];
+// find customer by id 
+$current_customer = $customer->find_by_id($wallet->customer_id);
+if(!$current_customer){
+    echo json_encode(array('message'=>'errorCustomer'));
+    die();
+}
+$wallet->amount = 0;
+$wallet->phone_number = $current_customer['phone_number'];
 $d = new DateTime();
 $wallet->created_date = $d->format('Y-m-d');
 $wallet->created_user_id = $_POST['user_id'];
