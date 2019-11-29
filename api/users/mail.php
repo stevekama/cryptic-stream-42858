97a74c4556dc5df2354203dd816a12d3236b02d1
stevekama <stevekamahertz@gmail.com>
2019-11-29 11:45:17 +0300

    connect to gmail

--- api/users/mail.php
+++ api/users/mail.php
@@ -12,7 +12,16 @@ require '../../vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-    
+    /// Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'stevekamahertz@gmail.com';                     // SMTP username
+    $mail->Password   = 'stevekama';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
