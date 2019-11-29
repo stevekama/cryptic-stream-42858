9c861eec979e43ab2c409ee29da428f684de7155
stevekama <stevekamahertz@gmail.com>
2019-11-29 11:26:11 +0300

    mail form

---
+++ public/users/mail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mail Form</title>
+</head>
+<body>
+    <form action="http://34.215.237.227/api/api/users/mail.php" method="post">
+        <input type="text" name="mail">
+        <br><br>
+        <input type="submit" value="send">
+    </form>
+</body>
+</html>
