4f5bc4eb4105ba4f355a169cd9f9c1631d30f248
stevekama <stevekamahertz@gmail.com>
2019-12-02 11:03:21 +0300

    test mail class

--- api/users/mail.php
+++ api/users/mail.php
@@ -6,8 +6,17 @@ header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 include_once '../../models/initialization.php';
 
-$mail = new SendMail();
+// Instantiation and passing `true` enables exception
+$mail = new PHPMailer(true);
 
-$mail->mail();
+$sendMail = new SendMail($mail);
+
+$sendMail->send_mail();
--- models/send_mail.php
+++ models/send_mail.php
@@ -1,30 +1,27 @@
 <?php 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 require_once('initialization.php');
 
-// Instantiation and passing `true` enables exception
-$mail = new PHPMailer(true);
-
 class SendMail{
-    
-    public function mail(){
-        global $mail;
+    private $mail;
 
+    public function __construct($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    public function send_mail(){
+        
         try{
             // Server settings
             // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.mailtrap.io';                       // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'f3bfa0269a5ef9';                     // SMTP username
-            $mail->Password   = '8acbed906d4b44';                               // SMTP password
-            $mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-            $mail->Port       = 25;                                    // TCP port to connect to
+            $this->mail->isSMTP();                                            // Send using SMTP
+            $this->mail->Host       = 'smtp.mailtrap.io';                       // Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $this->mail->Username   = 'f3bfa0269a5ef9';                     // SMTP username
+            $this->mail->Password   = '8acbed906d4b44';                               // SMTP password
+            $this->mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $this->mail->Port       = 25;                                    // TCP port to connect to
 
         }catch (Exception $e) {
             return $mail->ErrorInfo;
