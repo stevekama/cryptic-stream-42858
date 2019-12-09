1adad4290f85b4356a9aebc5def4f3a51d5bffc6
stevekama <stevekamahertz@gmail.com>
2019-12-09 04:51:27 +0300

    dashboard graph

--- public/index.php
+++ public/index.php
@@ -44,7 +44,7 @@ require_once('layouts/systems/header.php'); ?>
           <div class="nav-tabs-custom">
             <!-- Tabs within a box -->
             <ul class="nav nav-tabs pull-right">
-              <li class="active"><a href="#salesChart" data-toggle="tab">Area</a></li>
+              <li class="active"><a href=".chart" data-toggle="tab">Area</a></li>
               <li class="pull-left header"><i class="fa fa-inbox"></i> My Transactions</li>
             </ul>
             <div class="tab-content no-padding">
