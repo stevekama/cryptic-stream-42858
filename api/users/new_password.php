015b2314ee540015d62ac40bb711e01046efedd0
stevekama <stevekamahertz@gmail.com>
2019-12-11 10:24:37 +0300

    email error

--- api/users/new_password.php
+++ api/users/new_password.php
@@ -5,6 +5,12 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 include_once '../../models/initialization.php';
 
 // initialize user 
