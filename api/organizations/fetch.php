5541cf0caca6a95aea950f616160287c7643b996
stevekama <stevekamahertz@gmail.com>
2019-12-27 13:08:52 +0300

    order by

--- api/organizations/fetch.php
+++ api/organizations/fetch.php
@@ -12,22 +12,22 @@ $conn = $database->connect();
 
 $query = "";
 $output = array();
-$query .= "SELECT * FROM api.users WHERE type_id = '2' ";
-if(isset($_POST["search"]["value"])){
-   $query .= "AND fullnames LIKE '%".$_POST["search"]["value"]."' ";
-   $query .= "OR phone LIKE '%".$_POST["search"]["value"]."' ";
-   $query .= "OR email LIKE '%".$_POST["search"]["value"]."' ";
-}
+$query .= "SELECT * FROM api.users WHERE type_id = '2' ORDER BY id DESC";
+// if(isset($_POST["search"]["value"])){
+//    $query .= "AND fullnames LIKE '%".$_POST["search"]["value"]."' ";
+//    $query .= "OR phone LIKE '%".$_POST["search"]["value"]."' ";
+//    $query .= "OR email LIKE '%".$_POST["search"]["value"]."' ";
+// }
 
-if(isset($_POST["order"])){
-   $query .= "ORDER BY ".$_POST['order']['0']['column']." ".$_POST['order']['0']['dir']." ";
-}else{
-   $query .= "ORDER BY id DESC ";
-}
+// if(isset($_POST["order"])){
+//    $query .= "ORDER BY ".$_POST['order']['0']['column']." ".$_POST['order']['0']['dir']." ";
+// }else{
+//    $query .= "ORDER BY id DESC ";
+// }
 
-if($_POST["length"] != -1){
-   $query .= "LIMIT " . $_POST['start'] . ", " . $_POST['length'];
-}
+// if($_POST["length"] != -1){
+//    $query .= "LIMIT " . $_POST['start'] . ", " . $_POST['length'];
+// }
 
 $statement = $conn->prepare($query);
 $statement->execute();
