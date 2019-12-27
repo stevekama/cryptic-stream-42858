1064f49804000543fa821710e2a06eb68463d479
stevekama <stevekamahertz@gmail.com>
2019-12-27 12:29:53 +0300

    execute query

--- api/organizations/fetch.php
+++ api/organizations/fetch.php
@@ -18,3 +18,16 @@ if(isset($_POST["search"]["value"])){
    $query .= 'OR phone LIKE "%'.$_POST["search"]["value"].'%"';
    $query .= 'OR email LIKE "%'.$_POST["search"]["value"].'%") ';
 }
+
+if(isset($_POST["order"])){
+   $query .= 'ORDER BY '.$_POST['order']['0']['column'].' '.$_POST['order']['0']['dir'].' ';
+}else{
+   $query .= 'ORDER BY id DESC ';
+}
+
+if($_POST["length"] != -1){
+   $query .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+}
+
+$statement = $conn->prepare($query);
+$statement->execute();
