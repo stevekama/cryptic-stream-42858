9a7e3a72e0c54fbb929f61a61c31d4bf7edd97ed
stevekama <stevekamahertz@gmail.com>
2020-02-07 15:21:33 +0300

    base url

--- models/functions.php
+++ models/functions.php
@@ -1,13 +1,8 @@
 <?php 
 
 function base_url(){
-    if(isset($_SERVER['HTTPS'])){
-        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
-    }
-    else{
-        $protocol = 'http';
-    }
-    return $protocol . "://" . $_SERVER['HTTP_HOST'] .dirname($_SERVER['REQUEST_URI']).'/api/';
+    $url = "http://34.215.237.227/api/";
+    return $url;
 }
 
 function redirect_to($new_location){
