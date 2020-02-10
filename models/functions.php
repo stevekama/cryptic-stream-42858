0bb7b56c2881c138959cd52a2945d856b0a61aa9
stevekama <stevekamahertz@gmail.com>
2020-02-10 14:50:20 +0300

    load payment methods

--- models/functions.php
+++ models/functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 function base_url(){
+    // $url = "http://localhost/cryptic-stream-42858/";
     $url = "http://34.215.237.227/api/";
     return $url;
 }
--- public/dist/js/base_url.js
+++ public/dist/js/base_url.js
@@ -1,1 +1,2 @@
 const base_url = window.location.origin+'/api/';
+// const base_url = window.location.origin+'/cryptic-stream-42858/';
